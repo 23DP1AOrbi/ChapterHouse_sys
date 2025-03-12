128c81af2f00e73e1ddf563e8244c1878502c56f
Anna Laima Orbidane <166118293+23DP1AOrbi@users.noreply.github.com>
2025-03-12 13:08:01 +0000

    Added (kinda) plan, collection of books list for csv and started on book & bookshop classes

---
+++ src/main/java/lv/rvt/Book.java
@@ -0,0 +1,17 @@
+package lv.rvt;
+
+public class Book {
+    private String name;
+    private String author;
+    private int year;
+    private String ageCategory;
+
+    public Book(String name, String author, int year, String ageCategory) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.ageCategory = ageCategory;
+    }
+
+    
+}
